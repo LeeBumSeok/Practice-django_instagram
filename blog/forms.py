4f2f4af2395958af5fc64964cb758cda3cf04ae3
leebumseok <qpwoeiru6486@gmail.com>
2020-04-19 06:36:09 +0900

    implement signup and comment

--- blog/forms.py
+++ blog/forms.py
@@ -1,12 +1,29 @@
 from django import forms
-
-from .models import Post
+from .models import Post, Comment
+from django.contrib.auth.models import User
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = (
-            'title',
-            'text',
-        )
+        fields = ('title', 'text',)
+
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('author', 'text',)
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+
+def clean_username(self):
+    username = self.cleaned_data['username']
+    if User.objects.filter(username=username).exists():
+        raise forms.ValidationError('아이디가 이미 사용중입니다')
+    return username
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
-from django.utils import timezone
-from .models import Post
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import PostForm
+from django.utils import timezone
+from .models import Post, Comment
+from .forms import PostForm, UserForm
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from .forms import PostForm, CommentForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django import forms
+
+
+# Create your views here.
 
 
 def post_list(request):
@@ -16,13 +24,14 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -30,16 +39,80 @@ def post_new(request):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
-        form = PostForm(instance=post)
+        form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+@login_required
+def post_draft_list(request):
+    posts = Post.objects.filter(
+        published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
+
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+def signup(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        # if User.objects.filter(username=request.POST["username"]).exists():
+        #     return forms.Widget(attrs=True)
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(
+                username=request.POST["username"], password=request.POST['password1'])
+            login(request, user)
+            return redirect('/')
+
+    return render(request, 'blog/registration/signup.html')
